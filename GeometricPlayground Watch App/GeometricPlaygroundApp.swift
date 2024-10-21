d832f5daefbd0719dd59dfa53d29a5bc415ceb78
Wesley Grubbs <wes@pitchinteractive.com>
2024-10-21 07:51:39 -0700

    Initial Commit

---
+++ GeometricPlayground Watch App/GeometricPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricPlaygroundApp.swift
+//  GeometricPlayground Watch App
+//
+//  Created by Wesley Grubbs on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GeometricPlayground_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeometricPlayground/GeometricPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricPlaygroundApp.swift
+//  GeometricPlayground
+//
+//  Created by Wesley Grubbs on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GeometricPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
